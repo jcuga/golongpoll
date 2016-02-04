8c2d437dad15f194a3678fc2977bbaab74ff1beb
jcuga <jcuga@users.noreply.github.com>
2016-02-03 23:29:00 -0500

    adding unit tests for events and priority_queue

--- events_test.go
+++ events_test.go
@@ -8,18 +8,25 @@ import (
 )
 
 func Test_eventBuffer_QueueEvent(t *testing.T) {
+	test_start := time.Now()
 	buffer := eventBuffer{
 		list.New(),
 		10, // max buffer size
-		timeToEpochMilliseconds(time.Now()),
+		timeToEpochMilliseconds(test_start),
 	}
 	var events = []lpEvent{
-		lpEvent{timeToEpochMilliseconds(time.Now().Add(-10 * time.Second)), "red", "some string data 1"},
-		lpEvent{timeToEpochMilliseconds(time.Now().Add(-5 * time.Second)), "blue", "some string data 2"},
-		lpEvent{timeToEpochMilliseconds(time.Now()), "red", "some string data 3"}}
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-10 * time.Second)), "red", "some string data 1"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-5 * time.Second)), "blue", "some string data 2"},
+		lpEvent{timeToEpochMilliseconds(test_start), "red", "some string data 3"}}
 	for i := range events {
 		if err := buffer.QueueEvent(&events[i]); err != nil {
 			t.Errorf("Event queue failed when it shouldn't have. Tried to queue: %q", events[i])
+		}
+		// oldestEventTime should be the oldest event in the buffer, which is
+		// the first event we added
+		if buffer.oldestEventTime != events[0].Timestamp {
+			t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+				buffer.oldestEventTime, events[0].Timestamp)
 		}
 	}
 	// Verify data queued in expected order
@@ -45,18 +52,19 @@ func Test_eventBuffer_QueueEvent(t *testing.T) {
 }
 
 func Test_eventBuffer_QueueEvent_MaxBufferReached(t *testing.T) {
+	test_start := time.Now()
 	max := 10         // how large our buffer is
 	moreThanMax := 55 // how many events to queue
 	buffer := eventBuffer{
 		list.New(),
 		max, // max buffer size
-		timeToEpochMilliseconds(time.Now()),
+		timeToEpochMilliseconds(test_start),
 	}
 	// Create moreThanMax events
 	events := make([]lpEvent, 0)
 	for i := 0; i < moreThanMax; i++ {
 		events = append(events, lpEvent{
-			timeToEpochMilliseconds(time.Now().Add(time.Duration(i) * time.Second)),
+			timeToEpochMilliseconds(test_start.Add(time.Duration(i) * time.Second)),
 			"banana",
 			fmt.Sprintf("some string data %d", i)})
 	}
@@ -73,6 +81,13 @@ func Test_eventBuffer_QueueEvent_MaxBufferReached(t *testing.T) {
 	// Verify that the last max-many events are what's left in the buffer
 	if buffer.List.Len() != max {
 		t.Errorf("eventBuffer list length was: %d, expected: %d.", buffer.List.Len(), max)
+	}
+	// Oldest event time should be the timestamp of the last event in the buffer
+	// which is event index 45 since 55 were added but our max buffer caps at
+	// 10, so we only have events 45-54, index 45 is the oldest
+	if buffer.oldestEventTime != events[45].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, events[45].Timestamp)
 	}
 	// we should see only the last max-many items, so start our comparison
 	// offset at that point:
@@ -93,10 +108,11 @@ func Test_eventBuffer_QueueEvent_MaxBufferReached(t *testing.T) {
 }
 
 func Test_eventBuffer_QueueEvent_InvalidInput(t *testing.T) {
+	test_start := time.Now()
 	buffer := eventBuffer{
 		list.New(),
 		10, // max buffer size
-		timeToEpochMilliseconds(time.Now()),
+		timeToEpochMilliseconds(test_start),
 	}
 	// You can't queue nil.  And trying to do so shouldn't change the buffer.
 	if err := buffer.QueueEvent(nil); err == nil {
@@ -107,29 +123,39 @@ func Test_eventBuffer_QueueEvent_InvalidInput(t *testing.T) {
 		t.Errorf("Event list was modified after a failed queue.  "+
 			"Expected len: 0, got: %d", buffer.List.Len())
 	}
+	// this value should remain unchanged:
+	if buffer.oldestEventTime != timeToEpochMilliseconds(test_start) {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
 }
 
 func Test_eventBuffer_GetEventsSince(t *testing.T) {
+	test_start := time.Now()
 	buffer := eventBuffer{
 		list.New(),
 		10, // max buffer size
-		timeToEpochMilliseconds(time.Now()),
+		timeToEpochMilliseconds(test_start),
 	}
 	// Three of the events in our buffer occurred after our since param
-	since := time.Now().Add(-9 * time.Second)
+	since := test_start.Add(-9 * time.Second)
 	var events = []lpEvent{
-		lpEvent{timeToEpochMilliseconds(time.Now().Add(-12 * time.Second)), "red", "some string data 1"},
-		lpEvent{timeToEpochMilliseconds(time.Now().Add(-11 * time.Second)), "blue", "some string data 2"},
-		lpEvent{timeToEpochMilliseconds(time.Now().Add(-8 * time.Second)), "red", "some string data 3"},
-		lpEvent{timeToEpochMilliseconds(time.Now().Add(-5 * time.Second)), "blue", "some string data 4"},
-		lpEvent{timeToEpochMilliseconds(time.Now()), "red", "some string data 5"}}
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-12 * time.Second)), "red", "some string data 1"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-11 * time.Second)), "blue", "some string data 2"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-8 * time.Second)), "red", "some string data 3"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-5 * time.Second)), "blue", "some string data 4"},
+		lpEvent{timeToEpochMilliseconds(test_start), "red", "some string data 5"}}
 	for i := range events {
 		if err := buffer.QueueEvent(&events[i]); err != nil {
 			t.Errorf("Event queue failed when it shouldn't have. Tried to queue: %q", events[i])
 		}
 	}
+	if buffer.oldestEventTime != events[0].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
 	// Get all events since 9 seconds ago (last 3 events)
-	gotEvents, err := buffer.GetEventsSince(since, false) // TODO: add tests for doDelete = true
+	gotEvents, err := buffer.GetEventsSince(since, false)
 	if err != nil {
 		t.Fatalf("Got error %q from GetEventsSince with since: %v", err, since)
 	}
@@ -144,6 +170,77 @@ func Test_eventBuffer_GetEventsSince(t *testing.T) {
 		if gotEvents[i] != events[i+2] {
 			t.Fatalf("Expected: %q, got: %q", events[i+2], gotEvents[i])
 		}
+	}
+	// Remains unchanged:
+	if buffer.oldestEventTime != events[0].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
+}
+
+func Test_eventBuffer_GetEventsSince_deleteFetchedEvents(t *testing.T) {
+	// Now try the same test but with deleteFetchedEvents arg set to true
+	// and confirm fetched events were removed
+	test_start := time.Now()
+	buffer := eventBuffer{
+		list.New(),
+		10, // max buffer size
+		timeToEpochMilliseconds(test_start),
+	}
+	// Three of the events in our buffer occurred after our since param
+	since := test_start.Add(-9 * time.Second)
+	var events = []lpEvent{
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-12 * time.Second)), "red", "some string data 1"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-11 * time.Second)), "blue", "some string data 2"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-8 * time.Second)), "red", "some string data 3"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-5 * time.Second)), "blue", "some string data 4"},
+		lpEvent{timeToEpochMilliseconds(test_start), "red", "some string data 5"}}
+	for i := range events {
+		if err := buffer.QueueEvent(&events[i]); err != nil {
+			t.Errorf("Event queue failed when it shouldn't have. Tried to queue: %q", events[i])
+		}
+	}
+	if buffer.oldestEventTime != events[0].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
+	// Get all events since 9 seconds ago (last 3 events)
+	// NOTE: this time we specify that we should delete said fetched events
+	gotEvents, err := buffer.GetEventsSince(since, true)
+	if err != nil {
+		t.Fatalf("Got error %q from GetEventsSince with since: %v", err, since)
+	}
+	if len(gotEvents) != 3 {
+		t.Fatalf("GetEventsSince returned unexpected number of events. "+
+			"Expected: %d, Got: %d", 3, len(gotEvents))
+	}
+	// Confirm the event's we got are the ones we expect:
+	for i := range gotEvents {
+		// i is 0, 1, 2
+		// The event's we expect are events[2], [3], [4]
+		if gotEvents[i] != events[i+2] {
+			t.Fatalf("Expected: %q, got: %q", events[i+2], gotEvents[i])
+		}
+	}
+	// Remains unchanged.  It's not worth updating the oldest time when removing
+	// via this function because depending on the since argument you may or may
+	// not be removing events at the end.  Rather than adding the logic to
+	// conditionally update oldest event time, we leave it be.
+	// This is being lazy, but does not break any other behavior.
+	if buffer.oldestEventTime != events[0].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
+	// Now confirm that the buffer had thos events removed:
+	if buffer.List.Len() != 2 {
+		t.Errorf("buffer had unexpected number of events.  was: %d, expected: %d.",
+			buffer.List.Len(), 2)
+	}
+	if buffer.List.Front().Value.(*lpEvent) != &events[1] {
+		t.Errorf("buffer had unexpected event at front. expected events[1].")
+	}
+	if buffer.List.Front().Next().Value.(*lpEvent) != &events[0] {
+		t.Errorf("buffer had unexpected event at second item. expected events[0].")
 	}
 }
 
@@ -167,7 +264,7 @@ func Test_eventBuffer_GetEventsSince_AllEvents(t *testing.T) {
 		}
 	}
 	// Get all events since 30 seconds ago (all 5 events)
-	gotEvents, err := buffer.GetEventsSince(since, false) // TODO: add test for doDelete == true
+	gotEvents, err := buffer.GetEventsSince(since, false)
 	if err != nil {
 		t.Fatalf("Got error %q from GetEventsSince with since: %v", err, since)
 	}
@@ -203,7 +300,7 @@ func Test_eventBuffer_GetEventsSince_NoEvents(t *testing.T) {
 		}
 	}
 	// Get all events since ~0 seconds ago  (no events)
-	gotEvents, err := buffer.GetEventsSince(since, false) // TODO: add test for doDelete == true
+	gotEvents, err := buffer.GetEventsSince(since, false)
 	if err != nil {
 		t.Fatalf("Got error %q from GetEventsSince with since: %v", err, since)
 	}
@@ -222,7 +319,7 @@ func Test_eventBuffer_GetEventsSince_EmptyBuffer(t *testing.T) {
 	// any events in the last 30 seconds
 	since := time.Now().Add(-30 * time.Second)
 	// No events to get.  Not an error, but just no Event results
-	gotEvents, err := buffer.GetEventsSince(since, false) // TODO: add test for doDelete == True
+	gotEvents, err := buffer.GetEventsSince(since, false)
 	if err != nil {
 		t.Fatalf("Got error %q from GetEventsSince with since: %v", err, since)
 	}
@@ -239,7 +336,7 @@ func Test_eventBuffer_GetEventsSince_InvalidItems(t *testing.T) {
 		timeToEpochMilliseconds(time.Now()),
 	}
 	buffer.List.PushBack("some string.  clearly not an Event type")
-	events, err := buffer.GetEventsSince(time.Now(), false) // TODO: add test for doDelete == true
+	events, err := buffer.GetEventsSince(time.Now(), false)
 	// This buffer is hosed because there is non-Event data in it.
 	// All calls to GetEventsSince should fail.
 	if err == nil {
@@ -250,4 +347,113 @@ func Test_eventBuffer_GetEventsSince_InvalidItems(t *testing.T) {
 	}
 }
 
-// TODO: test eventBuffer.oldestEventTime
+func Test_eventBuffer_DeleteEventsOlderThan_Trivial(t *testing.T) {
+	test_start := time.Now()
+	buffer := eventBuffer{
+		list.New(),
+		10, // max buffer size
+		timeToEpochMilliseconds(test_start),
+	}
+
+	err := buffer.DeleteEventsOlderThan(timeToEpochMilliseconds(test_start.Add(1 * time.Second)))
+	if err != nil {
+		t.Errorf("DeleteEventsOlderThan returned non-nil error: %v", err)
+	}
+	if buffer.List.Len() != 0 {
+		t.Errorf("Expected empty list, got %d items in list.", buffer.List.Len())
+	}
+}
+
+func Test_eventBuffer_DeleteEventsOlderThan_InvalidData(t *testing.T) {
+	test_start := time.Now()
+	buffer := eventBuffer{
+		list.New(),
+		10, // max buffer size
+		timeToEpochMilliseconds(test_start),
+	}
+	// Corrupt list by adding something that is not an lpEvent:
+	buffer.List.PushBack("asdf")
+	// Confirm calls to operate on this list are now errors
+	err := buffer.DeleteEventsOlderThan(timeToEpochMilliseconds(test_start.Add(1 * time.Second)))
+	if err == nil {
+		t.Errorf("DeleteEventsOlderThan returned nil error when non-nil error was expected.")
+	}
+	if buffer.List.Len() != 1 {
+		t.Errorf("Expected 1 item list, got %d items in list.", buffer.List.Len())
+	}
+}
+
+func Test_eventBuffer_DeleteEventsOlderThan(t *testing.T) {
+	test_start := time.Now()
+	buffer := eventBuffer{
+		list.New(),
+		10, // max buffer size
+		timeToEpochMilliseconds(test_start),
+	}
+	var events = []lpEvent{
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-12 * time.Second)), "red", "some string data 1"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-11 * time.Second)), "blue", "some string data 2"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-8 * time.Second)), "red", "some string data 3"},
+		lpEvent{timeToEpochMilliseconds(test_start.Add(-5 * time.Second)), "blue", "some string data 4"},
+		lpEvent{timeToEpochMilliseconds(test_start), "red", "some string data 5"}}
+	for i := range events {
+		if err := buffer.QueueEvent(&events[i]); err != nil {
+			t.Errorf("Event queue failed when it shouldn't have. Tried to queue: %q", events[i])
+		}
+	}
+	if buffer.oldestEventTime != events[0].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
+	// Scenario 1: ask to delete older than a time older than the oldest event,
+	// which means nothing gets deleted.
+	if err := buffer.DeleteEventsOlderThan(timeToEpochMilliseconds(test_start.Add(-13 * time.Second))); err != nil {
+		t.Errorf("Unexpected error during DeleteEventsOlderThan: %v", err)
+	}
+	// No events removed
+	if buffer.List.Len() != len(events) {
+		t.Errorf("Unexpected number of events left in buffer. was: %d, expected: %d.",
+			buffer.List.Len(), len(events))
+	}
+	// oldestEventTime unchanged
+	if buffer.oldestEventTime != events[0].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
+
+	// Scenario 2: ask to delete older than some time half way thru the events
+	// so some of them are deleted
+	if err := buffer.DeleteEventsOlderThan(timeToEpochMilliseconds(test_start.Add(-10 * time.Second))); err != nil {
+		t.Errorf("Unexpected error during DeleteEventsOlderThan: %v", err)
+	}
+	// Oldest 2 events removed
+	if buffer.List.Len() != 3 {
+		t.Errorf("Unexpected number of events left in buffer. was: %d, expected: %d.",
+			buffer.List.Len(), 3)
+	}
+	// oldestEventTime updated to the oldest event that is now in buffer
+	if buffer.oldestEventTime != events[2].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
+
+	// Scenario 3: ask to delete older than some time newer than all events,
+	// so all events are deleted.
+	// NOTE: asking to delete at time == newest event time,
+	// this also tests that we are inclusive in our boundary, using <= not just <
+	if err := buffer.DeleteEventsOlderThan(timeToEpochMilliseconds(test_start)); err != nil {
+		t.Errorf("Unexpected error during DeleteEventsOlderThan: %v", err)
+	}
+	// Remaining 3 events removed
+	if buffer.List.Len() != 0 {
+		t.Errorf("Unexpected number of events left in buffer. was: %d, expected: %d.",
+			buffer.List.Len(), 0)
+	}
+	// as events are removed oldestEventTime is set to the next event,
+	// so now that we're empty, our oldestEventTime member just mirrors the
+	// oldest event that was removed
+	if buffer.oldestEventTime != events[4].Timestamp {
+		t.Errorf("eventBuffer.oldestEventTime was: %v, expected: %v.",
+			buffer.oldestEventTime, test_start)
+	}
+}
--- priority_queue_test.go
+++ priority_queue_test.go
@@ -1,9 +1,188 @@
 package golongpoll
 
 import (
+	"container/heap"
+	"container/list"
 	"testing"
+	"time"
 )
 
+func Test_priorityQueue_Len(t *testing.T) {
+	pq := make(priorityQueue, 0)
+	if pq.Len() != 0 {
+		t.Errorf("priorityQueue had unexpected Len().  was: %d, expected: %d",
+			pq.Len(), 0)
+	}
+	// add an item
+	now_ms := timeToEpochMilliseconds(time.Now())
+	buf := &eventBuffer{
+		list.New(),
+		100,
+		now_ms,
+	}
+	expiringBuf := &expiringBuffer{
+		eventBuffer_ptr: buf,
+		category:        "some random category",
+		priority:        now_ms,
+	}
+	heap.Push(&pq, expiringBuf)
+	if pq.Len() != 1 {
+		t.Errorf("priorityQueue had unexpected Len().  was: %d, expected: %d",
+			pq.Len(), 1)
+	}
+	// add another
+	buf2 := &eventBuffer{
+		list.New(),
+		100,
+		now_ms,
+	}
+	expiringBuf2 := &expiringBuffer{
+		eventBuffer_ptr: buf2,
+		category:        "some different category",
+		priority:        now_ms,
+	}
+	heap.Push(&pq, expiringBuf2)
+	if pq.Len() != 2 {
+		t.Errorf("priorityQueue had unexpected Len().  was: %d, expected: %d",
+			pq.Len(), 2)
+	}
+	// Remove an item
+	pq.Pop()
+	if pq.Len() != 1 {
+		t.Errorf("priorityQueue had unexpected Len().  was: %d, expected: %d",
+			pq.Len(), 1)
+	}
+	pq.Pop()
+	if pq.Len() != 0 {
+		t.Errorf("priorityQueue had unexpected Len().  was: %d, expected: %d",
+			pq.Len(), 0)
+	}
+}
+
 func Test_priorityQueue(t *testing.T) {
-	// TODO: test this sucker.
+	now_ms := timeToEpochMilliseconds(time.Now())
+	pq := make(priorityQueue, 0)
+	heap.Init(&pq)
+
+	if _, e := pq.peakTopPriority(); e == nil {
+		t.Errorf("No error returned when calling peakTopPriority on an empty priorityQueue")
+	}
+
+	buf1 := &eventBuffer{
+		list.New(),
+		100,
+		now_ms,
+	}
+	expiringBuf1 := &expiringBuffer{
+		eventBuffer_ptr: buf1,
+		category:        "some random category",
+		priority:        10003, // lower number is a higher ( priority 1 > priority 2)
+	}
+	heap.Push(&pq, expiringBuf1)
+	if p, e := pq.peakTopPriority(); e != nil {
+		t.Errorf("Error returned when calling peakTopPriority: %v", e)
+	} else {
+		if p != 10003 {
+			t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+				10003, p)
+		}
+	}
+
+	buf2 := &eventBuffer{
+		list.New(),
+		100,
+		now_ms,
+	}
+	expiringBuf2 := &expiringBuffer{
+		eventBuffer_ptr: buf2,
+		category:        "some random category",
+		priority:        10001,
+	}
+	heap.Push(&pq, expiringBuf2)
+	if p, e := pq.peakTopPriority(); e != nil {
+		t.Errorf("Error returned when calling peakTopPriority: %v", e)
+	} else {
+		if p != 10001 {
+			t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+				10001, p)
+		}
+	}
+
+	buf3 := &eventBuffer{
+		list.New(),
+		100,
+		now_ms,
+	}
+	expiringBuf3 := &expiringBuffer{
+		eventBuffer_ptr: buf3,
+		category:        "some random category",
+		priority:        10051,
+	}
+	heap.Push(&pq, expiringBuf3)
+	if p, e := pq.peakTopPriority(); e != nil {
+		t.Errorf("Error returned when calling peakTopPriority: %v", e)
+	} else {
+		if p != 10001 {
+			t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+				10001, p)
+		}
+	}
+
+	buf4 := &eventBuffer{
+		list.New(),
+		100,
+		now_ms,
+	}
+	expiringBuf4 := &expiringBuffer{
+		eventBuffer_ptr: buf4,
+		category:        "some random category",
+		priority:        10011,
+	}
+	heap.Push(&pq, expiringBuf4)
+	if p, e := pq.peakTopPriority(); e != nil {
+		t.Errorf("Error returned when calling peakTopPriority: %v", e)
+	} else {
+		if p != 10001 {
+			t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+				10001, p)
+		}
+	}
+
+	if item := heap.Pop(&pq).(*expiringBuffer); item != expiringBuf2 {
+		t.Errorf("Expected popped item != expiringBuf2")
+	}
+	if p, _ := pq.peakTopPriority(); p != 10003 {
+		t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+			10003, p)
+	}
+
+	if item := heap.Pop(&pq).(*expiringBuffer); item != expiringBuf1 {
+		t.Errorf("Expected popped item != expiringBuf1")
+	}
+	if p, _ := pq.peakTopPriority(); p != 10011 {
+		t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+			10011, p)
+	}
+
+	// Now stir the pot by updating expiringBuf3 to higher priority than expiringBuf4
+	pq.updatePriority(expiringBuf3, 10008)
+	if p, _ := pq.peakTopPriority(); p != 10008 {
+		t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+			10008, p)
+	}
+
+	if item := heap.Pop(&pq).(*expiringBuffer); item != expiringBuf3 {
+		t.Errorf("Expected popped item != expiringBuf3")
+	}
+	if p, _ := pq.peakTopPriority(); p != 10011 {
+		t.Errorf("Unexpected peakTopPriority result.  was: %d, expected: %d.",
+			10011, p)
+	}
+	if item := heap.Pop(&pq).(*expiringBuffer); item != expiringBuf4 {
+		t.Errorf("Expected popped item != expiringBuf4")
+	}
+
+	if _, e := pq.peakTopPriority(); e == nil {
+		t.Errorf("No error returned when calling peakTopPriority on an empty priorityQueue")
+	}
 }
