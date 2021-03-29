f5e25fc28f11898bee783b40086a59a1f5a1cf24
Justin <justin@examle.com>
2021-03-28 21:34:58 -0400

    adding chatbot example with go and js clients

--- examples/chatbot/chatbot.go
+++ examples/chatbot/chatbot.go
@@ -1,3 +1,119 @@
+// This example creates a dummy chatbot while demonstrating the following features:
+// 1) Golang client used by the trivial chatbot
+// 2) Javascript client used by UI.
+package main
+
+import (
+	"fmt"
+	"github.com/jcuga/golongpoll"
+	"github.com/jcuga/golongpoll/client"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+func main() {
+
+	// TODO: go thru and comment all this
+	// TODO: comment how this is contrived (no multi user/segmentation)
+	// TODO: comment how one could do that and add auth/checks (involve actual payload not plain string too)
+	manager, err := golongpoll.StartLongpoll(golongpoll.Options{
+		LoggingEnabled: true,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create manager: %q", err)
+	}
+
+	http.HandleFunc("/chatbot", chatBotExampleHomepage)
+	http.HandleFunc("/chatbot/events", manager.SubscriptionHandler)
+	http.HandleFunc("/chatbot/send", getSendHandler(manager))
+	fmt.Println("Serving webpage at http://127.0.0.1:8101/chatbot")
+
+	go beChatbot(manager)
+	http.ListenAndServe("127.0.0.1:8101", nil)
+}
+
+func beChatbot(lpManager *golongpoll.LongpollManager) {
+	u, err := url.Parse("http://127.0.0.1:8101/chatbot/events")
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := client.NewClient(client.ClientOptions{
+		Url:            *u,
+		Category:       "to-chatbot",
+		LoggingEnabled: true,
+	})
+	if err != nil {
+		fmt.Println("FAILED TO CREATE LONGPOLL CLIENT: ", err)
+		return
+	}
+
+	for event := range c.Start(time.Now()) {
+		msg := event.Data.(string)
+		reply := "I see you said: \"" + msg + "\""
+		lpManager.Publish("from-chatbot", reply)
+	}
+
+	// Only happens if ...
+	// TODO: is this when client stops, manager stops? either? too tired right now...
+	fmt.Println("Chatbot golongpoll.Client out of events, stopping.")
+}
+
+func getSendHandler(manager *golongpoll.LongpollManager) func(w http.ResponseWriter, r *http.Request) {
+	// Creates closure that captures the LongpollManager
+	return func(w http.ResponseWriter, r *http.Request) {
+		data := r.URL.Query().Get("data")
+		if len(data) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Missing required URL param 'data'."))
+			return
+		}
+		manager.Publish("to-chatbot", data)
+	}
+}
+
+func chatBotExampleHomepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `
+<html>
+<head>
+    <title>golongpoll chatbot</title>
+</head>
+<body>
+<script>
+function send() {
+	var data = $("#send-input").val();
+	if (data.length == 0) {
+		alert("input cannot be empty");
+		return;
+	}
+
+	var jqxhr = $.get( "/chatbot/send", { data: data })
+		.done(function() {
+			// TODO: style this, add formatted timestamp
+			$("#conversation").append("<p>ME: " + data + "</p>");	
+		})
+		.fail(function() {
+		  alert( "post request failed" );
+		});
+}
+</script>
+	<h1>golongpoll chatbot</h1>
+	<div id="conversation" />
+	<input id="send-input"type="text" /><button id="publish-btn" onclick="send()">Send</button>
+	<!-- NOTE: jquery is NOT requried to use golongpoll or the golongpoll javascript client.
+	Including here for shorter/lazier example. -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<!-- TODO: update to master/not feature branch once merged. -->
+<script>
+
+
+
+
+
+
+
 var golongpoll = {
     newClient: function ({
             url,
@@ -163,3 +279,31 @@ var golongpoll = {
         return client;
     }
 };
+
+
+
+
+
+</script>
+<script>
+	var client = golongpoll.newClient({
+		url: "/chatbot/events",
+		category: "from-chatbot",
+		// NOTE: without setting sinceTime here, defaults to only new events (sinceTime of now)
+		loggingEnabled: true,
+		onEvent: function (event) {
+			// TODO: style this, add formatted timestamp from event.timestamp
+			$("#conversation").append("<p>BOT: " + event.data + "</p>");
+		},
+	});
+
+	// newClient returns null if failed.
+	if (!client) {
+		alert("Failed to create golongpoll client.");
+	} else {
+		console.log(client);
+	}
+</script>
+</body>
+</html>`)
+}
